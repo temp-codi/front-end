5de33a48d5a382e0a652e51a96f133945efeea96
pol-dev-shinroo <manus.dream.language@gmail.com>
2023-01-18 20:06:42 +0900

    twin macro 세팅 성공!

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
-import styles from "@/styles/Home.module.css";
+import { styled } from "twin.macro";
 
 export default function Home() {
-  return <>Hello World</>;
+  return (
+    <Wrapper className="border-8 border-red-600">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  height: 100px;
+  width: 100px;
+`;
