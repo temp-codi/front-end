4a60dd8e05abf7a594d6f0e90b5700191dfe3137
gusdas <gustmd213@naver.com>
2023-01-29 19:53:01 +0900

    react-query test

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,20 @@ import { styled } from 'twin.macro';
 import TestAtom from '@/pages/components/atoms/TestAtom';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import useLocation from '@/pages/api/location';
 export default function Home() {
   const router = useRouter();
   const { pid } = router.query;
-
+  const getApi = () => {
+    useLocation;
+  };
   return (
     <Wrapper className="border-8 border-red-600">
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Link href="/testAtom" id="testAtom">
         Home
       </Link>
+      <button onClick={getApi}>get api</button>
       <TestAtom></TestAtom>
     </Wrapper>
   );
