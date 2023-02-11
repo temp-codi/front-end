db7f34a67a1d23ec7ec3927ac2959c0336471dae
gusdas <gustmd213@naver.com>
2023-02-11 17:46:55 +0900

    왼쪽 organisms 완성

--- components/molecules/CodiBtn.tsx
+++ components/molecules/CodiBtn.tsx
@@ -9,7 +9,7 @@ interface Props {
 const CodiBtn = ({ firstCategory, secondCategory }: Props) => {
   if (secondCategory) {
     return (
-      <Flex
+      <Btn
         direction="row"
         align="center"
         justify="between"
@@ -21,11 +21,11 @@ const CodiBtn = ({ firstCategory, secondCategory }: Props) => {
         <Text className=" text-center pr-5 font-bold text-sm" as="p">
           {secondCategory}
         </Text>
-      </Flex>
+      </Btn>
     );
   } else {
     return (
-      <Flex
+      <Btn
         direction="row"
         align="center"
         justify="center"
@@ -34,7 +34,7 @@ const CodiBtn = ({ firstCategory, secondCategory }: Props) => {
         <Text className="font-bold text-xl" as="p">
           {firstCategory}
         </Text>
-      </Flex>
+      </Btn>
     );
   }
 };
